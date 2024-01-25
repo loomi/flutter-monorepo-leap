2c78958055e35ea43dbf4938d72f86f8cdf594c5
lucavps <vieira@loomi.com.br>
2024-01-25 19:08:41 -0300

    Add icon and splash screen

---
+++ commons/lib/app/utils/assets.dart
@@ -0,0 +1,15 @@
+import '../helpers/assets_helper.dart';
+
+class Assets {
+  static final String leapIcon = getAssetImageUrl(
+    "leap_icon.png",
+  );
+
+  static final String alertIcon = getAssetVectorUrl(
+    "alert.svg",
+  );
+
+  static final String searchIcon = getAssetVectorUrl(
+    "search.svg",
+  );
+}
